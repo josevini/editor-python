843819e5eb106538f11782301314153bfda23450
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-03 22:38:23 -0300

    Update modExibe.py

---
+++ modExibe.py
@@ -0,0 +1,28 @@
+import time
+
+def atrasar(msg='', seg=1.0):
+    print(msg)
+    time.sleep(seg)
+
+def desenha(simb='', qtd=0):
+    print(simb * qtd)
+
+def mensagem(msg='', char='-'):
+    tamanho = len(msg) * 3
+    desenha(char, tamanho)
+    print(f'{msg.center(tamanho)}')
+    desenha(char, tamanho)
+
+def mudaCor(text='', cor='preto'):
+    cores = {
+        'black': '\033[30m',
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'yellow': '\033[33m',
+        'blue': '\033[34m',
+        'purple': '\033[35m',
+        'gray': '\033[37m'
+    }
+    abre = cores[cor]
+    fecha = '\033[m'
+    return f'{abre}{text}{fecha}'
