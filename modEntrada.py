d417ad28ac84e9642327acf04ac16b3fc5842e89
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-03 22:38:19 -0300

    Update modEntrada.py

---
+++ modEntrada.py
@@ -0,0 +1,22 @@
+from modExibe import *
+
+def entradaTexto(msg=''):
+    valor = input(msg)
+    return valor
+
+def pergunta(msg=''):
+    resp = input(msg).lower()
+    if resp == 's':
+        return True
+    elif resp == 'n':
+        return False
+    else:
+        atrasar(mudaCor('Opção inválida!', 'red'))
+
+def entrada(msg=''):
+    while True:
+        valor = input(msg)
+        try:
+            return int(valor)
+        except Exception as erro:
+            atrasar(mudaCor('Digite um número válido!', 'red'))
