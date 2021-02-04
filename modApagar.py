a792267ac1b04e781ede86305f1a77c79688b7e3
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-04 16:22:32 -0300

    Update modApagar.py

--- modApagar.py
+++ modApagar.py
@@ -3,6 +3,17 @@ import os.path
 from modEntrada import *
 from modExibe import *
 
+def apagarArquivo():
+    nome, ext = os.path.splitext(entradaTexto('Informe o nome do arquivo: '))
+    filename = (nome + ext) if ext else (nome + '.txt')
+    try:
+        atrasar(mudaCor('Processando...', 'yellow'), 1.3)
+        os.remove(filename)
+        atrasar(mudaCor(f'Apagando o arquivo {filename}...', 'yellow'), 1.5)
+        atrasar(mudaCor(f'Arquivo {filename} apagado!', 'green'), 1.3)
+    except FileNotFoundError as erro:
+        atrasar(mudaCor('Arquivo não encontrado!', 'red'), 1.3)
+
 def menuApagar():
     while True:
         desenha('-', 42)
@@ -16,6 +27,6 @@ def menuApagar():
             atrasar(mudaCor('Cancelando...', 'red'), 1.3)
             break
         elif op == 1:
-            print('Apagar arquivo...')
+            apagarArquivo()
         elif op == 2:
             print('Apagar pasta...')
