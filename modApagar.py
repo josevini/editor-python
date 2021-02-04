76d76f7d649e4be68e2092b8c34ebfcd230b0e52
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-04 16:51:11 -0300

    Update modApagar.py

--- modApagar.py
+++ modApagar.py
@@ -6,12 +6,21 @@ from modExibe import *
 def apagarArquivo():
     nome, ext = os.path.splitext(entradaTexto('Informe o nome do arquivo: '))
     filename = (nome + ext) if ext else (nome + '.txt')
-    try:
-        atrasar(mudaCor('Processando...', 'yellow'), 1.3)
-        os.remove(filename)
-        atrasar(mudaCor(f'Apagando o arquivo {filename}...', 'yellow'), 1.5)
-        atrasar(mudaCor(f'Arquivo {filename} apagado!', 'green'), 1.3)
-    except FileNotFoundError as erro:
+    atrasar(mudaCor('Processando...', 'yellow'), 1.3)
+    if os.path.exists(filename):
+        if os.path.getsize(filename) > 0:
+            continuar = pergunta(mudaCor('O arquivo não está vazio, quer continuar? [s/n]: ', 'yellow'))
+            if continuar:
+                os.remove(filename)
+                atrasar(mudaCor(f'Apagando o arquivo {filename}...', 'yellow'), 1.5)
+                atrasar(mudaCor(f'Arquivo {filename} apagado!', 'green'), 1.3)
+            else:
+                atrasar(mudaCor('Nenhum arquivo foi apagado!', 'red'), 1.3)
+        else:
+            os.remove(filename)
+            atrasar(mudaCor(f'Apagando o arquivo {filename}...', 'yellow'), 1.5)
+            atrasar(mudaCor(f'Arquivo {filename} apagado!', 'green'), 1.3)
+    else:
         atrasar(mudaCor('Arquivo não encontrado!', 'red'), 1.3)
 
 def menuApagar():
