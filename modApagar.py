3a16c1070192b100424564c7278352d711f72912
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-03 23:13:17 -0300

    Update modApagar.py

--- modApagar.py
+++ modApagar.py
@@ -3,4 +3,19 @@ import os.path
 from modEntrada import *
 from modExibe import *
 
-
+def menuApagar():
+    while True:
+        desenha('-', 42)
+        print("""Deseja apagar um arquivo ou uma pasta?
+1 - Arquivo
+2 - Pasta
+0 - Cancelar""")
+        desenha('-', 42)
+        op = intervalo(entrada('Escolha uma opção: '), 0, 2)
+        if op == 0:
+            atrasar(mudaCor('Cancelando...', 'red'), 1.3)
+            break
+        elif op == 1:
+            print('Apagar arquivo...')
+        elif op == 2:
+            print('Apagar pasta...')
