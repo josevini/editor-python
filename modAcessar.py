2c4cd5662215c201336acd25d03e210298469eda
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-08 21:34:00 -0300

    Update modAcessar.py

--- modAcessar.py
+++ modAcessar.py
@@ -2,3 +2,12 @@ import os
 import os.path
 from modEntrada import *
 from modExibe import *
+
+def menuAcessar():
+    while True:
+        desenha('-', 42)
+        print("""Deseja acessar um arquivo ou pasta?
+1 - Arquivo
+2 - Pasta
+0 - Cancelar""")
+        break
