d516ab7b6fd982fb325895088b3db929e190e1c1
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-09 22:53:22 -0300

    Update modAcessar.py

--- modAcessar.py
+++ modAcessar.py
@@ -9,4 +9,10 @@ def menuAcessar():
         geraMenu('arquivo', 'pasta', msg='Deseja acessar um arquivo ou pasta?')
         desenha('-', 42)
         op = intervalo(entrada('Escolha uma opção: '), 0, 2)
+        if op == 0:
+            break
+        elif op == 1:
+            pass
+        elif op == 2:
+            pass
         break
