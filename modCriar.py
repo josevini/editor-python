94029a316fb509de5d77ac86956cf299b9e744c3
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-17 21:33:09 -0300

    Update modCriar.py

--- modCriar.py
+++ modCriar.py
@@ -4,16 +4,16 @@ from modEntrada import *
 from modExibe import *
 
 def criarArquivo():
-    total = entrada('Quantos arquivos: ')
+    total = getNumber('Quantos arquivos: ')
     while total > 0:
         desenha('-', 42)
-        nome, ext = os.path.splitext(entradaTexto('Informe o nome do arquivo: '))
-        continuar = pergunta(mudaCor('Deseja prosseguir com a ação? [s/n]: ', 'yellow'))
+        nome, ext = os.path.splitext(getText('Informe o nome do arquivo: '))
+        continuar = question(mudaCor('Deseja prosseguir com a ação? [s/n]: ', 'yellow'))
         if continuar:
             filename = (nome + ext) if ext else (nome + '.txt')
             atrasar(mudaCor('Processando...', 'yellow'))
             if os.path.exists(filename):
-                resp = pergunta(mudaCor('Arquivo encontrado! Quer sobrescrever? [s/n]: ', 'yellow'))
+                resp = question(mudaCor('Arquivo encontrado! Quer sobrescrever? [s/n]: ', 'yellow'))
                 if resp:
                     atrasar(mudaCor(f'Criando arquivo {filename}...', 'yellow'), 1.3)
                     file = open(filename, 'w', encoding='utf-8')
@@ -30,8 +30,8 @@ def criarArquivo():
 
 def criarPasta():
     while True:
-        dirname = entradaTexto('Informe o nome da pasta: ')
-        continuar = pergunta(mudaCor('Deseja prosseguir com a ação? [s/n]: ', 'yellow'))
+        dirname = getText('Informe o nome da pasta: ')
+        continuar = question(mudaCor('Deseja prosseguir com a ação? [s/n]: ', 'yellow'))
         if continuar:
             atrasar(mudaCor(f'Criando a pasta {dirname}...', 'yellow'), 1.3)
             try:
@@ -49,7 +49,7 @@ def menuCriar():
         desenha('-', 42)
         geraMenu('arquivo', 'pasta', msg='Deseja criar um arquivo ou pasta?')
         desenha('-', 42)
-        op = intervalo(entrada('Escolha uma opção: '), 0, 2)
+        op = intervalo(getNumber('Escolha uma opção: '), 0, 2)
         if op == 0:
             atrasar(mudaCor('Cancelando...', 'red'))
             break
