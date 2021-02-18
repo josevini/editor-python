e7c69581f5472c61de4f5f319af25d936c5d1ac2
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-17 21:39:38 -0300

    Update modEditar.py

--- modEditar.py
+++ modEditar.py
@@ -8,18 +8,22 @@ def editarArquivo():
     geraMenu('Editar conteúdo', 'Renomear', msg='Opções disponíveis para edição')
     desenha('-', 42)
     op = intervalo(getNumber('Escolha a opção: '), 0, 2)
+    if op == 0:
+        atrasar(mudaCor('Cancelando...', 'red'))
 
 def editarPasta():
     pass
 
 def menuEditar():
-    desenha('-', 42)
-    geraMenu('arquivo', 'pasta', msg='Deseja editar um arquivo ou pasta?')
-    desenha('-', 42)
-    op = intervalo(getNumber('Escolha uma opção: '), 0, 2)
-    if op == 0:
-        atrasar(mudaCor('Cancelando...', 'red'))
-    elif op == 1:
-        editarArquivo()
-    elif op == 2:
-        editarPasta()
+    while True:
+        desenha('-', 42)
+        geraMenu('arquivo', 'pasta', msg='Deseja editar um arquivo ou pasta?')
+        desenha('-', 42)
+        op = intervalo(getNumber('Escolha uma opção: '), 0, 2)
+        if op == 0:
+            atrasar(mudaCor('Cancelando...', 'red'))
+            break
+        elif op == 1:
+            editarArquivo()
+        elif op == 2:
+            editarPasta()
