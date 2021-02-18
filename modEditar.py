726b37012b83bac79c94df167a2ff6b14319b701
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-17 21:33:11 -0300

    Update modEditar.py

--- modEditar.py
+++ modEditar.py
@@ -3,7 +3,23 @@ import os.path
 from modEntrada import *
 from modExibe import *
 
+def editarArquivo():
+    desenha('-', 42)
+    geraMenu('Editar conteúdo', 'Renomear', msg='Opções disponíveis para edição')
+    desenha('-', 42)
+    op = intervalo(getNumber('Escolha a opção: '), 0, 2)
+
+def editarPasta():
+    pass
+
 def menuEditar():
     desenha('-', 42)
     geraMenu('arquivo', 'pasta', msg='Deseja editar um arquivo ou pasta?')
     desenha('-', 42)
+    op = intervalo(getNumber('Escolha uma opção: '), 0, 2)
+    if op == 0:
+        atrasar(mudaCor('Cancelando...', 'red'))
+    elif op == 1:
+        editarArquivo()
+    elif op == 2:
+        editarPasta()
