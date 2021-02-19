0a5b8e490f31079473b9335c8722ff8d43b29284
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-19 20:42:16 -0300

    Create modEdit.py

---
+++ modEdit.py
@@ -0,0 +1,33 @@
+import os
+import os.path
+from modInput import *
+from modShow import *
+
+def editFile():
+    while True:
+        toDesign('-', 42)
+        createMenu('Renomear', 'Editar conteúdo', msg='Opções disponíveis para edição')
+        toDesign('-', 42)
+        op = interval(getNumber('Escolha a opção: '), 0, 2)
+        if op == 0:
+            delay(changeColor('Cancelando...', 'red'))
+        elif op == 1:
+            pass
+        elif op == 2:
+            pass
+def editarPasta():
+    pass
+
+def menuEditar():
+    while True:
+        toDesign('-', 42)
+        createMenu('arquivo', 'pasta', msg='Deseja editar um arquivo ou pasta?')
+        toDesign('-', 42)
+        op = interval(getNumber('Escolha uma opção: '), 0, 2)
+        if op == 0:
+            delay(changeColor('Cancelando...', 'red'))
+            break
+        elif op == 1:
+            editFile()
+        elif op == 2:
+            editarPasta()
