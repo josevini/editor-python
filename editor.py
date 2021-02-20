1962c62b1139bc55ab233f2be4fd4d634b0de30c
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-19 22:21:42 -0300

    Update editor.py

--- editor.py
+++ editor.py
@@ -6,7 +6,7 @@ from modEdit import *
 def menu():
     while True:
         message('MENU PRINCIPAL')
-        createMenu('criar', 'apagar', 'acessar', 'editar', stop='sair')
+        createMenu('criar', 'editar', 'acessar', 'apagar', stop='sair')
         toDesign('-', 42)
         op = interval(getNumber('Escolha uma opção: '), 0, 4)
         if op == 0:
@@ -14,8 +14,8 @@ def menu():
         elif op == 1:
             menuCriar()
         elif op == 2:
-            menuDelete()
+            menuEditar()
         elif op == 3:
             menuAccess()
         elif op == 4:
-            menuEditar()
+            menuDelete()
